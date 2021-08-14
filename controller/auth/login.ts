7fab6c75b70959dbfa15251b4d3b566f7dd92209
Abhinav0909 <abhinavkarforma2@gmail.com>
2021-08-14 22:11:53 +0530

    🐛 Fix api bug

--- controller/auth/login.ts
+++ controller/auth/login.ts
@@ -4,18 +4,20 @@ import * as bcrypt from 'bcrypt';
 import { ObjectId } from 'mongodb';
 import * as jwt from 'jsonwebtoken';
 interface Data{
-  _id:ObjectId,email:string,password:string
+  _id: ObjectId,
+  email: string,
+  password: string
 }
 export const logInController = async (user: LogIn) => {
-  const data: Data = await (await database()).collection('customers').findOne({ email: user.email }) as Data;
-  if (data === null) {
-    throw {
-      message:"Password doesn't match"
+  const data = await (await database()).collection('customers').findOne({ email: user.email }) as Data;
+    if (data === null) {
+      throw {
+        status:404,
+        message: "User doesn't exist"
+      }
     }
-
-  }
-  else {
-    return bcrypt.compareSync(user.password, data.password) ? jwt.sign({ id: data._id }, process.env.JWT_SECRET as string) :null;
+    else {
+      return bcrypt.compareSync(user.password, data.password) ? jwt.sign({ id: data._id }, process.env.JWT_SECRET as string) : null;
     
   }
 }
