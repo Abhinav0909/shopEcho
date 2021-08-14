e65cc97c90b10ab4eae266057751f683cb01c6a0
Abhinav0909 <abhinavkarforma2@gmail.com>
2021-08-14 16:17:54 +0530

    📦 Update file structure

---
+++ controller/auth/login.ts
@@ -0,0 +1,21 @@
+import database from "../../common/database";
+import { LogIn } from "../../models/logIn";
+import * as bcrypt from 'bcrypt';
+import { ObjectId } from 'mongodb';
+import * as jwt from 'jsonwebtoken';
+interface Data{
+  _id:ObjectId,email:string,password:string
+}
+export const logInController = async (user: LogIn) => {
+  const data: Data = await (await database()).collection('customers').findOne({ email: user.email }) as Data;
+  if (data === null) {
+    throw {
+      message:"Password doesn't match"
+    }
+
+  }
+  else {
+    return bcrypt.compareSync(user.password, data.password) ? jwt.sign({ id: data._id }, process.env.JWT_SECRET as string) :null;
+    
+  }
+}
---
+++ pages/api/auth/router/login.ts
@@ -0,0 +1,44 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import Joi from "joi";
+import { logInController } from "../../../../controller/auth/login";
+
+const logInHandler = async(req:NextApiRequest,res:NextApiResponse) => {
+  try {
+    if (req.method === 'POST') {
+      const schema = Joi.object({
+        email: Joi.string().email().required(),
+        password: Joi.string().min(6).max(12).required(),
+      })
+      const { value, error } = schema.validate(req.body);
+      if (error) {
+        throw {
+          status: 422,
+          message: "Validation error"
+        }
+      } else {
+        logInController(value).then((val) => {
+          if (val === null) {
+            res.status(203).json({
+              message: 'Login failed',
+            })
+          } else {
+            res.status(200).json({
+              message: 'Login Succesfull',
+              token:val,
+            })
+          }
+        }).catch((error) => {
+          res.status(error.status || 401).json({
+           message:error.message || 'Password need to be checked',
+         })
+        })
+      }
+    
+    }
+  }catch(err) {
+    res.status(err.status || 500).json({
+      message:err.message || "Internal error occured",
+    })
+  }
+}
+export default logInHandler;
