ff6f6e06a47fbf545a8a08297d0ea873299b82e0
Abhinav0909 <abhinavkarforma2@gmail.com>
2021-08-16 00:22:01 +0530

    🐛 Remove warnings

--- controller/otp/otpGenerator.ts
+++ controller/otp/otpGenerator.ts
@@ -1,28 +1,29 @@
 import { ObjectId } from "mongodb";
 import { customAlphabet } from "nanoid";
 import database from "../../common/database";
-const nanoid = customAlphabet('1234567890', 4)
 interface Data{
   _id:ObjectId
   email:string,
 }
+const nanoid = customAlphabet('1234567890', 4);
 export const otpGeneratorController = async(email:string) => {
   try {
-    const data:Data = await(await database()).collection('customers').findOne({ email:email })as Data;
-    if (data === null) {
-      return 'Check your email again';
+    const data = await (await database()).collection('customers').findOne({email:email }) as Data;
+    if (!data) {
+      throw { message: 'There is no account linked to this mail id' }
     }
-    else {
-      const otpCreator = await (await database()).collection('otp').findOne({ email:email });
-      if (otpCreator === null) {
+    else{
+      const otpCreator = await (await database()).collection('otp').findOne({ email: email });
+      if (!otpCreator) {
         await (await database()).collection('otp').findOneAndDelete({ email: email });
-      } else {
+      } 
         const otpCreation = nanoid();
-        await (await database()).collection('otp').insertOne({ otp: otpCreation, email:email, expireAt: new Date() })
+        await (await database()).collection('otp').insertOne({ otp: otpCreation, email: email, expireAt: new Date() })
         return 'Successful';
-      }
     }
-  } catch (err) {
-    return 'Something went wrong';
+  } catch(err) {
+    throw {
+      message:err.message || 'An error occured'
+    }
   }
 }
