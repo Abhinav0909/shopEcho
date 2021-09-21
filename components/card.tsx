4dadcaef21fb5d6680deb61b70834c1356fb1f6b
Abhinav0909 <abhinavkarforma2@gmail.com>
2021-09-21 11:30:07 +0530

    ♻️ Improved naming style

---
+++ components/card.tsx
@@ -0,0 +1,17 @@
+/* eslint-disable @next/next/link-passhref */
+import Link from "next/link";
+/* eslint-disable @next/next/no-img-element */
+const Card = ({fetchData}:{fetchData:{_id:string,topic:string}}) => {
+    return (
+        <div className='lg:w-1/3 xl:w-1/4 mx-auto my-10 md:w-1/2 w-3/4 '>
+        <Link href={`Lab/${fetchData._id}`}>
+                <div className='my-auto hover:-translate-y-1 transform rounded-lg bg-card  bg-cover  bg-no-repeat flex justify-center  mx-auto md:h-56 md:w-56 h-48 w-48 border-4 border-blue-500 '>
+                    <p className='my-auto text-blue-100 md:text-xl text-lg font-semibold font-mono  '>{fetchData.topic}</p>
+                    </div>
+            </Link>
+            </div>
+       
+    )
+}
+
+export default Card;
